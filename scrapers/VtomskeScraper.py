55eb397e4a217b51be1c9acf9b9672d606383cbc
Ilia Trof <prettyfox1337@gmail.com>
2026-09-17 15:07:08 +0300

    added refactored VtomskeScraper class in scrapers/VtomskeScraper

--- scrapers/VtomskeScraper.py
+++ scrapers/VtomskeScraper.py
@@ -1,58 +1,56 @@
 import re
-import requests
 
-from tqdm import tqdm
+import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 from scrapers.BaseScraper import BaseScraper
 
+
 class VtomskeScraper(BaseScraper):
 
-    BASE_URL = 'https://vtomske.ru'
+    BASE_URL = "https://vtomske.ru"
 
-    def __init__(self,
-                 scraper_name: str):
+    def __init__(self, scraper_name: str):
 
         super().__init__(scraper_name)
 
-    def _get_bs_object(self,
-                       url: str) -> BeautifulSoup:
+    def _get_bs_object(self, url: str) -> BeautifulSoup:
         """
         Метод для получения объекта BeautifulSoup
 
         Args:
             url: str
-        
+
         Returns:
             BeautifulSoup | None: Объект BeautifulSoup или None при status_code != 200
         """
 
-        response = requests.get(url = url)
+        response = requests.get(url=url)
 
         if response.status_code != 200:
 
-            print(f'The request was unsuccessfull')
-            print(f'Status code: {response.status_code}')
-            print(f'Response: text: {response.text}')
+            print(f"The request was unsuccessfull")
+            print(f"Status code: {response.status_code}")
+            print(f"Response: text: {response.text}")
 
             return None
 
-        bs_object = BeautifulSoup(response.text, 'html.parser')
+        bs_object = BeautifulSoup(response.text, "html.parser")
 
         return bs_object
 
-    def _parse_headline(self,
-                        news_url: str) -> str | None:
-        '''
+    def _parse_headline(self, news_url: str) -> str | None:
+        """
         Метод для сбора заголовка с конкретной новостной статьи
-        '''
+        """
 
-        bs_object = self._get_bs_object(url = news_url)
+        bs_object = self._get_bs_object(url=news_url)
 
         if bs_object is None:
             return None
 
-        news_headline = bs_object.find('h1').text
+        news_headline = bs_object.find("h1").text
 
         if not news_headline:
             return None
@@ -60,82 +58,80 @@ class VtomskeScraper(BaseScraper):
         else:
             return news_headline
 
-    def _parse_news_body(self,
-                         news_url: str) -> str | None:
+    def _parse_news_body(self, news_url: str) -> str | None:
         """
         Метод для получения тела новости
 
         Args:
             news_url (str): URL-новости
-        
+
         Returns:
-            str | None: Текст новости либо None. 
+            str | None: Текст новости либо None.
         """
 
-        bs_object = self._get_bs_object(url = news_url)
+        bs_object = self._get_bs_object(url=news_url)
 
         if bs_object is None:
 
             return None
 
-        page_content = bs_object.find('div', class_='material-content')
+        page_content = bs_object.find("div", class_="material-content")
 
         if not page_content:
             return None
 
-        all_text_material = page_content.find_all(re.compile(r'^(h[1-6]|p|ul)$')) # Получаем все возможные теги
+        all_text_material = page_content.find_all(
+            re.compile(r"^(h[1-6]|p|ul)$")
+        )  # Получаем все возможные теги
 
-        news_body = '\n'.join([x.text for x in all_text_material])
+        news_body = "\n".join([x.text for x in all_text_material])
 
         return news_body
 
-    def _parse_publishing_date(self,
-                               news_url: str) -> str | None:
-        '''
+    def _parse_publishing_date(self, news_url: str) -> str | None:
+        """
         Метод для получения даты (datetime) публикации новости в формате строки
 
         Args:
             news_url (str): Ссылка на страницу новости.
-        
+
         Returns:
             str | None: datetime новости в формате str или None
-        '''
+        """
 
-        bs_object = self._get_bs_object(url = news_url)
+        bs_object = self._get_bs_object(url=news_url)
 
         if bs_object is None:
             return bs_object
 
-        material_information = bs_object.find('div', class_='material-info')
+        material_information = bs_object.find("div", class_="material-info")
 
         if not material_information:
             return None
 
-        time_tag = material_information.find('time', class_='material-date')
+        time_tag = material_information.find("time", class_="material-date")
 
-        dt_str = time_tag.get('datetime')
+        dt_str = time_tag.get("datetime")
 
         return dt_str
 
-    def _parse_author(self,
-                      news_url: str) -> str | None:
-        '''
+    def _parse_author(self, news_url: str) -> str | None:
+        """
         Метод для получения автора новостной статьи
-        '''
+        """
 
-        bs_object = self._get_bs_object(url = news_url)
+        bs_object = self._get_bs_object(url=news_url)
 
-        material_tag = bs_object.find('div', class_='material-info')
+        material_tag = bs_object.find("div", class_="material-info")
 
         if not material_tag:
             return None
 
-        author = material_tag.find('a', class_='material-author').text
+        author = material_tag.find("a", class_="material-author").text
 
         return author
 
-    def _get_page_news(self,
-                       page_url: str) -> list[str] | None:
+    def _get_page_news(self, page_url: str) -> list[str] | None:
         """
         Метод для получения всех ссылок на новости со страницы.
 
@@ -147,7 +143,7 @@ class VtomskeScraper(BaseScraper):
 
         """
 
-        bs_object = self._get_bs_object(url = page_url)
+        bs_object = self._get_bs_object(url=page_url)
 
         links_element = bs_object.find_all("a", class_="lenta_material")
 
@@ -155,18 +151,17 @@ class VtomskeScraper(BaseScraper):
 
             return None
 
-        links = [self.BASE_URL + str(x.get('href')) for x in links_element]
+        links = [self.BASE_URL + str(x.get("href")) for x in links_element]
 
         return links
 
-    def _parse_single_article(self,
-                              news_url: str) -> dict[str, str | None]:
+    def _parse_single_article(self, news_url: str) -> dict[str, str | None]:
         """
         Метод выполняющий парсинг одной новостной статьи.
 
         Args:
             news_url (str): URL конкретной новостной статьи
-        
+
         Returns:
             dict: Словарь со следующими ключами:
             - headline. Заголовок новостной статьи.
@@ -180,34 +175,32 @@ class VtomskeScraper(BaseScraper):
             "news_body": None,
             "publishing_date": None,
             "author": None,
-            "url": news_url
+            "url": news_url,
         }
-
 
         try:
 
-            parsed_headline = self._parse_headline(news_url = news_url)
-            parsed_body = self._parse_news_body(news_url = news_url)
-            parsed_publishing_date = self._parse_publishing_date(news_url = news_url)
-            parsed_author = self._parse_author(news_url = news_url)
+            parsed_headline = self._parse_headline(news_url=news_url)
+            parsed_body = self._parse_news_body(news_url=news_url)
+            parsed_publishing_date = self._parse_publishing_date(news_url=news_url)
+            parsed_author = self._parse_author(news_url=news_url)
 
-            base_dict['headline'] = parsed_headline
-            base_dict['news_body'] = parsed_body
-            base_dict['publishing_date'] = parsed_publishing_date
-            base_dict['author'] = parsed_author
+            base_dict["headline"] = parsed_headline
+            base_dict["news_body"] = parsed_body
+            base_dict["publishing_date"] = parsed_publishing_date
+            base_dict["author"] = parsed_author
 
             return base_dict
 
         except Exception as e:
 
-            print(f'An unexpected Error occured during article parsing!')
-            print(f'Exception text: {e}')
+            print(f"An unexpected Error occured during article parsing!")
+            print(f"Exception text: {e}")
 
             return base_dict
 
-    def _get_next_page_link(self,
-                            page_url: str) -> str | None:
-        '''
+    def _get_next_page_link(self, page_url: str) -> str | None:
+        """
         Метод для получения следующего URL в конкретной тематики,
         т.е. кнопка 'Раньше'.
 
@@ -218,38 +211,36 @@ class VtomskeScraper(BaseScraper):
             # str | None: str, если кнопка присутсвует или None, если это последняя страница рубрики.
             *Примечание* Ссылка возвращается в следующем формате: '?down=1784357100',
             То есть данный элемент элемент необходимо конкатенировать с url целевой тематики.
-        '''
+        """
 
         bs_object = self._get_bs_object(page_url)
 
         if bs_object is None:
             return None
 
-        next_page_link = bs_object.find('a', class_='btn lenta_pager_next')
+        next_page_link = bs_object.find("a", class_="btn lenta_pager_next")
 
         if not next_page_link:
-            print('URL следующей страницы не найден!')
+            print("URL следующей страницы не найден!")
             return None
 
         else:
-            return next_page_link.get('href')
+            return next_page_link.get("href")
 
-    def parse_rubric(self,
-                     start_url: str,
-                     num_news: int) -> list[dict]:
-        '''
+    def parse_rubric(self, start_url: str, num_news: int) -> list[dict]:
+        """
         Метод для сбора желаемого количества новостей по конкретной тематике.
 
         Args:
             start_url (str): Начальный (стартовый) URL, с которого требуется начать поиск.
             num_news (int): Количество новостей, которое необходимо собрать.
-        
+
         Returns:
             list[dict]: Список словарей. Словари следует формату, описанному в методе _parse_single_article
-        
+
         *Примечание* Желательно в качестве start_url передавать начальную страницу рубрики, вроде: https://vtomske.ru/tag/russia
         Для более удобного перехода по страницам.
-        '''
+        """
 
         num_collected_news = 0
 
@@ -259,30 +250,33 @@ class VtomskeScraper(BaseScraper):
 
         while num_collected_news < num_news:
 
-            print(f'Идет сбор с страницы: {next_page_url}')
+            print(f"Идет сбор с страницы: {next_page_url}")
 
-            current_news_links = self._get_page_news(page_url = next_page_url)
+            current_news_links = self._get_page_news(page_url=next_page_url)
 
             for news_link in current_news_links:
 
-                news_dict = self._parse_single_article(news_url = news_link)
+                news_dict = self._parse_single_article(news_url=news_link)
 
                 collected_news.append(news_dict)
 
             num_collected_news = len(collected_news)
 
-            print(f'Количество собранных новостей: {len(collected_news)}')
+            print(f"Количество собранных новостей: {len(collected_news)}")
 
-            next_page_url = start_url + self._get_next_page_link(page_url = next_page_url)
+            next_page_url = start_url + self._get_next_page_link(page_url=next_page_url)
 
             if not next_page_url:
 
-                print('На текущей странице не найдено URL следующей страницы (кнопки Раньше), завершаю цикл...')
+                print(
+                    "На текущей странице не найдено URL следующей страницы (кнопки Раньше), завершаю цикл..."
+                )
                 break
 
-        print('Цикл сбора новостей успешно завершен!')
-        print(f'Количество собранных новостей: {len(collected_news)}')
+        print("Цикл сбора новостей успешно завершен!")
+        print(f"Количество собранных новостей: {len(collected_news)}")
         return collected_news
+
 
 class NewsParser:
     """
@@ -305,8 +299,8 @@ class NewsParser:
                 8. Спорт.
         """
 
-        print('Устаревший класс!')
-        print('Для сбора новостей используй VtomskeScraper!')
+        print("Устаревший класс!")
+        print("Для сбора новостей используй VtomskeScraper!")
 
         self.thematics_url = {
             "томск": "https://vtomske.ru/tag/tomsk",
--- scripts/vtomske_scraping.py
+++ scripts/vtomske_scraping.py
@@ -1,30 +1,41 @@
-#  Sample Parsing Script
+#  Скрипт актуализирован 17.09.2026
+# В соответствии с изменениями парсера. Новая версия - VtomskeScraper
 
 import pandas as pd
 
-from scrapers.VtomskeScraper import NewsParser
+from scrapers.VtomskeScraper import VtomskeScraper
+
+
+thematics_url = {
+    "томск": "https://vtomske.ru/tag/tomsk",
+    "россия": "https://vtomske.ru/tag/russia",
+    "мир": "https://vtomske.ru/tag/world",
+    "экономика": "https://vtomske.ru/tag/economics",
+    "политика": "https://vtomske.ru/tag/politics",
+    "происшествия": "https://vtomske.ru/tag/incident",
+    "авто": "https://vtomske.ru/tag/auto",
+    "спорт": "https://vtomske.ru/tag/sport",
+}
+
+scraper = VtomskeScraper(
+    scraper_name = 'VtomskeScraper'
+)
 
 n_news = int(input('Введите количество новостей, которые необходимо собрать: '))
 
-thematics = [
-    'томск',
-    'россия',
-    'мир',
-    'экономика',
-    'политика',
-    'происшествия',
-    'авто',
-    'спорт'
-]
+user_thematic = input('Введите название тематики (рубрики), по которой необходимо собрать новости: ').lower()
 
-for thema in thematics:
+thematic_url = thematics_url.get(user_thematic, None)
 
-    parser = NewsParser(thematic = thema)
+if not thematics_url:
+    print(f'Введена отутсвующая тематика: ```{user_thematic}```')
+    print(f'Список поддерживаемых тематик: {'\n'.join(list(thematics_url.keys()))}')
+    raise ValueError('Повторите ввод, используя поддерживаемую тематику')
 
-    result = parser.parse_thematic(n_news = n_news)
 
-    df = pd.DataFrame(result)
+resulting_news = scraper.parse_rubric(start_url = thematic_url,
+                                      num_news = n_news)
 
-    df.to_csv(f'data/{thema}.csv', index = False)
+df = pd.DataFrame(resulting_news)
 
-    print(f'Датасет по тематике {thema} сохранен!')
+df.to_csv(f'data/vtomske_data/{user_thematic}.csv', index = False) # Перед запуском убедитесь, что в директории data присутсвует поддиректория vtomske_data
